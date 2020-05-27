5237e4dc400674ec4f64275c1cb5bba2c4112107
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-05-27 11:17:48 +0400

    qw
    Signed-off-by: QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("hello world")
+# print("hello world")
+# print ("hello " + "srijan")
+##print string variable
+# greet="hello"
+# name=" srijan"
+# print(greet+name)
+
+#####Remainder#####
+remainder=1398%11
+print(remainder)
---
+++ your_name.py
@@ -0,0 +1,5 @@
+name = input("whats  your first name? \n")
+name2 = input("whats your last name? \n")
+print("Hi " + name + "\t" + name2)
+
+
